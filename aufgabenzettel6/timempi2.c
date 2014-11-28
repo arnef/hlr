833e420329cd47c974a5119644b5df879d98f8b0
Arne <git@arnefeil.de>
2014-11-28 17:59:26 +0100

    so derbe mit kommentar und so ein shit

--- aufgabenzettel6/timempi2.c
+++ aufgabenzettel6/timempi2.c
@@ -8,42 +8,52 @@
 
 int main (int argc, char** argv) {
     
+    // id des prozesses
     int rank;
+    // anzahl der prozesse
     int size;
-    int minimum[1];
+    int global_minimum_microseconds[1];  
+    int microseconds[1];
+
+    char message[45];
+    char hostname[5];
+    
+    struct timeval time;
+    struct tm* ptm;
+    char time_string[40];
+    
+    // rank sender und empfänger
+    int to;
+    int from;
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     
-    char message[45];
     MPI_Status status;
-        char hostname[5];
-        gethostname(hostname, sizeof hostname);
+    gethostname(hostname, sizeof hostname);
 
-        struct timeval time;
-        struct tm* ptm;
-        char time_string[40];
     
-        gettimeofday(&time, (struct timezone *) 0);
-        ptm = localtime(&time.tv_sec);
-        strftime(time_string, sizeof(time_string), "%Y-%m-%d %H:%M:%S", ptm);
-        int microsecond[1];
-        if (rank == 0) {
-            microsecond[0] = INT_MAX;
-        }
-        else {
-            microsecond[0] = (int) time.tv_usec; 
-        }
-        MPI_Reduce(microsecond, minimum, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
-    int to;
+    gettimeofday(&time, (struct timezone *) 0);
+    ptm = localtime(&time.tv_sec);
+    strftime(time_string, sizeof(time_string), "%Y-%m-%d %H:%M:%S", ptm);
     if (rank == 0) {
-        int from;
+        // falls der rank 0 ist soll die microsekunden nicht mit in
+        // reduce funktion betrachtet werden, da aber jeder prozess die
+        // funktion ausführen muss, setzten wir hier die microseconds auf int max value
+        microseconds[0] = INT_MAX;
+    }
+    else {
+        microseconds[0] = (int) time.tv_usec; 
+    }
+    MPI_Reduce(microseconds, global_minimum_microseconds, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+    
+    if (rank == 0) {
         for (from = 1; from < size; from++) {
             MPI_Recv (message, 45, MPI_CHAR, from, 0, MPI_COMM_WORLD, &status);
             printf("%s\n", message);
         }
-        printf("%d\n", minimum[0]);
+        printf("%d\n", global_minimum_microseconds[0]);
     }
     else {
         to = 0;
