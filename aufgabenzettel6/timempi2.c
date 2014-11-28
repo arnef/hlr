95d4f136f46ce59f5df7e234bb78741f8328240d
Arne <git@arnefeil.de>
2014-11-28 17:48:37 +0100

    aufg 2 done

--- aufgabenzettel6/timempi2.c
+++ aufgabenzettel6/timempi2.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <time.h>
+#include <limits.h>
 #include <mpi.h>
 
 
@@ -9,7 +10,7 @@ int main (int argc, char** argv) {
     
     int rank;
     int size;
-    
+    int minimum[1];
     
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -17,17 +18,6 @@ int main (int argc, char** argv) {
     
     char message[45];
     MPI_Status status;
-    int to;
-    if (rank == 0) {
-        int from;
-        for (from = 1; from < size; from++) {
-            MPI_Recv (message, 170, MPI_CHAR, from, 0, MPI_COMM_WORLD, &status);
-            printf("%s\n", message);
-        }
-    }
-    else {
-        to = 0;
-        //get_host_timestamp(message);
         char hostname[5];
         gethostname(hostname, sizeof hostname);
 
@@ -38,12 +28,31 @@ int main (int argc, char** argv) {
         gettimeofday(&time, (struct timezone *) 0);
         ptm = localtime(&time.tv_sec);
         strftime(time_string, sizeof(time_string), "%Y-%m-%d %H:%M:%S", ptm);
-    
+        int microsecond[1];
+        if (rank == 0) {
+            microsecond[0] = INT_MAX;
+        }
+        else {
+            microsecond[0] = (int) time.tv_usec; 
+        }
+        MPI_Reduce(microsecond, minimum, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+    int to;
+    if (rank == 0) {
+        int from;
+        for (from = 1; from < size; from++) {
+            MPI_Recv (message, 45, MPI_CHAR, from, 0, MPI_COMM_WORLD, &status);
+            printf("%s\n", message);
+        }
+        printf("%d\n", minimum[0]);
+    }
+    else {
+        to = 0;
         sprintf(message, "%s: %s.%d", hostname, time_string, (int) time.tv_usec);
-        MPI_Send (message, strlen(message), MPI_CHAR, to, 0, MPI_COMM_WORLD);
+        MPI_Send (message, 45, MPI_CHAR, to, 0, MPI_COMM_WORLD);
     }
     MPI_Barrier(MPI_COMM_WORLD); 
     printf("Rang %d beendet jetzt!\n", rank);
+    
     MPI_Finalize();
     return 0;
 }
